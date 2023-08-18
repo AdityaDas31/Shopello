46069b8f50ba17b90a9d00fad6979f1f72ab88f0
Aditya Das <adityadasofficial31@gmail.com>
2023-08-18 07:24:42 +0530

    navbar basic

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,12 @@
 import React,{Fragment} from 'react';
-import Navbar from './miscellaneous/navbar';
+//import Navbar from './miscellaneous/Navbar';
+import Navbar from './miscellaneous/header';
 
 const Home = () => {
   return (
     <Fragment>
       <Navbar/>
+      home
     </Fragment>
   )
 }
