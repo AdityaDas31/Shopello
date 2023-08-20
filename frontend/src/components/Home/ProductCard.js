8d7f5bd7d9dde458c01d0270bb474b512d30c40c
Aditya Das <adityadasofficial31@gmail.com>
2023-08-20 18:02:29 +0530

    product page

---
+++ frontend/src/components/Home/ProductCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Rating from '@material-ui/lab/Rating'
+
+
+
+const ProductCard = ({ product }) => {
+    const options = {
+        value: product.rating,
+        readOnly: true,
+        precision: 0.5,
+    };
+
+
+
+
+    return (
+        <Link className='productCard' to={product._id}>
+            <img src={product.images[0].url} alt={product.name} />
+            <p>{product.name}</p>
+            <div>
+               <Rating {...options} /> <span className='productCard-span'>(100 Reviews)</span>
+            </div>
+            <span>{product.price}</span>
+        </Link>
+    )
+}
+
+export default ProductCard;
