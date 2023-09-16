f8559a72726ab8b6aa152b07d7ba49fbcc320e11
Aditya Das <adityadasofficial31@gmail.com>
2023-09-16 10:46:21 +0530

    tooltip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'react-tooltip/dist/react-tooltip.css' 
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/Home/Home';
 import Products from './components/Product/Products';
