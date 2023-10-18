e51cea8f3e11262b03697b29c378074bed9b6711
Kaustav Ghosh <kaustavghosh444@gmail.com>
2023-10-18 20:29:20 +0530

    about

---
+++ frontend/src/components/miscellaneous/About/About.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import './About.css';
+import icon1 from "../../../images/truck.svg";
+import icon2 from "../../../images/bag.svg";
+import icon3 from "../../../images/support.svg";
+import icon4 from "../../../images/return.svg";
+import icon5 from "../../../images/about.jpg";
+import kaustav from "../../../images/Kaustav.jpg";
+import aditya from "../../../images/Aditya.jpg";
+import Navbar from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+const About = () => {
+    return (
+        <>
+            <Navbar />
+            <div className="why-choose-section">
+                <div className="container">
+                    <div className="row justify-content-between align-items-center">
+                        <div className="col-lg-6">
+                            <h2 className="section-title">Why Choose Us</h2>
+                            <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</p>
+
+                            <div className="row my-5">
+                                <div className="col-6 col-md-6">
+                                    <div className="feature">
+                                        <div className="icon">
+                                            <img src={icon1} alt="Icon" className="imf-fluid" />
+                                        </div>
+                                        <h3>Fast &amp; Free Shipping</h3>
+                                        <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
+                                    </div>
+                                </div>
+
+                                <div className="col-6 col-md-6">
+                                    <div className="feature">
+                                        <div className="icon">
+                                            <img src={icon2} alt="Icon" className="imf-fluid" />
+                                        </div>
+                                        <h3>Easy to Shop</h3>
+                                        <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
+                                    </div>
+                                </div>
+
+                                <div className="col-6 col-md-6">
+                                    <div className="feature">
+                                        <div className="icon">
+                                            <img src={icon3} alt="Icon" className="imf-fluid" />
+                                        </div>
+                                        <h3>24/7 Support</h3>
+                                        <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
+                                    </div>
+                                </div>
+
+                                <div className="col-6 col-md-6">
+                                    <div className="feature">
+                                        <div className="icon">
+                                            <img src={icon4} alt="Icon" className="imf-fluid" />
+                                        </div>
+                                        <h3>Hassle Free Returns</h3>
+                                        <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="col-lg-5">
+                            <div className="img-wrap">
+                                <img src={icon5} alt="Icon" className="img-fluid" />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="untree_co-section">
+                <div className="container">
+
+                    <div className="row mb-5">
+                        <div className="col-lg-5 mx-auto text-center">
+                            <h2 className="section-title">Our Team</h2>
+                        </div>
+                    </div>
+
+                    <div className="row">
+
+
+                        <div className="col-12 col-md-6 col-lg-3 mb-5 mb-md-0 link">
+                            <img src={aditya} className="img-fluid mb-5" alt="icon" />
+                            <h3><Link to="#"><span className="">Aditya</span> Das</Link ></h3>
+                            <span className="d-block position mb-4">MERN DEVELOPER</span>
+                            <p>Separated they live in.
+                                Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                            <p className="mb-0"><Link to="https://www.linkedin.com/in/aditya-das-513883222" className="more dark">Learn More <span className="icon-arrow_forward"></span></Link ></p>
+                        </div>
+
+                        <div className="col-12 col-md-6 col-lg-3 mb-5 mb-md-0 link">
+                            <img src={kaustav} className="img-fluid mb-5" alt="icon" />
+
+                            <h3><Link to="#"><span className="">Kaustav</span> Ghosh</Link ></h3>
+                            <span className="d-block position mb-4">MERN DEVELOPER</span>
+                            <p>Separated they live in.
+                                Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                            <p className="mb-0"><Link to="https://www.linkedin.com/in/kaustav-ghosh-b8a8b3222" className="more dark">Learn More <span className="icon-arrow_forward"></span></Link ></p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+
+    )
+}
+
+export default About
--- frontend/src/components/miscellaneous/Header/Header.js
+++ frontend/src/components/miscellaneous/Header/Header.js
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../images/logo.png";
 import './Navbar.css';
+import Modal from 'react-bootstrap/Modal';
 
-const Navbar = () => {
+const Header = () => {
+
+    const [show, setShow] = useState(false);
+    
     const [canvas, setCanvas] = useState("");
     const canvas__open = () => {
         if (canvas === "") {
@@ -13,14 +17,17 @@ const Navbar = () => {
         }
     }
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
 
     return (
         <>
             <div className={`offcanvas-menu-overlay ${canvas}`} onClick={canvas__open}></div>
             <div className={`offcanvas-menu-wrapper ${canvas}`} >
-                <div className="offcanvas__close" onClick={canvas__open}>+</div>
+            <div className="offcanvas__close" onClick={canvas__open}><i class="fa-solid fa-xmark"></i></div>
                 <ul className="offcanvas__widget">
-                    <li><i className="fas fa-search"></i></li>
+                <li><i className="fas fa-search" onClick={handleShow}></i></li>
                     <li><Link to="/Profile"><i class="fa-regular fa-user"></i>
                     </Link></li>
                     <li><Link to="/Cart"><i className="fas fa-shopping-cart"></i>
@@ -31,14 +38,15 @@ const Navbar = () => {
                 </div>
                 {/* <div id="mobile-menu-wrap"></div> */}
 
-                <div id="mobile-menu-wrap"><div class="slicknav_menu"><Link to="#" aria-haspopup="true" role="button" tabindex="0" class="slicknav_btn slicknav_collapsed" ><span class="slicknav_menutxt">MENU</span><span class="slicknav_icon"><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span></span></Link><nav class="slicknav_nav slicknav_hidden d-none" aria-hidden="true" role="menu" >
+                {/* <div id="mobile-menu-wrap"><div class="slicknav_menu"><Link to="#" aria-haspopup="true" role="button" tabindex="0" class="slicknav_btn slicknav_collapsed" ><span class="slicknav_menutxt">MENU</span><span class="slicknav_icon"><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span><span class="slicknav_icon-bar"></span></span></Link><nav class="slicknav_nav slicknav_hidden d-none" aria-hidden="true" role="menu" >
                     <ul>
                         <li class="active"><Link to="/" role="menuitem">Home</Link></li>
                         <li><Link to="#" role="menuitem">Women's</Link></li>
                         <li><Link to="#" role="menuitem">Men's</Link></li>
                         <li><Link to="#" role="menuitem">Shop</Link></li>
+                        <li><Link to="/About" role="menuitem">About Us</Link></li>
                         <li class="slicknav_parent slicknav_collapsed"><Link to="#" role="menuitem" aria-haspopup="true" tabindex="-1" class="slicknav_item slicknav_row" ><Link to="/Products">Products</Link>
-                            {/* <span class="slicknav_arrow">►</span> */}
+                            
                             </Link><ul class="dropdown slicknav_hidden d-none" role="menu" aria-hidden="true" >
                               
                             </ul>
@@ -50,7 +58,7 @@ const Navbar = () => {
                 <div className="offcanvas__auth">
                     <Link to="#">Login</Link>
                     <Link to="#">Register</Link>
-                </div>
+                </div> */}
             </div>
 
             <header className="header">
@@ -61,7 +69,7 @@ const Navbar = () => {
                                 <Link to="/"><img src={logo} alt="" style={{ width: 100 }} /></Link>
                             </div>
                         </div>
-                        <div className="col-xl-6 col-lg-7">
+                        {/* <div className="col-xl-6 col-lg-7">
                             <nav className="header__menu">
                                 <ul>
                                     <li className="active"><Link to="/">Home</Link></li>
@@ -69,17 +77,18 @@ const Navbar = () => {
                                     <li><Link to="#">Men's</Link></li>
                                     <li><Link to="#">Shop</Link></li>
                                     <li><Link to="/Products">Products</Link></li>
+                                    <li><Link to="/About">About Us</Link></li>
                                 </ul>
                             </nav>
-                        </div>
-                        <div className="col-lg-3">
+                        </div> */}
+                        <div className="col-lg-3 header__right_con">
                             <div className="header__right">
                                 {/* <div className="header__right__auth">
                                     <Link to="#">Login</Link>
                                     <Link to="#">Register</Link>
                                 </div> */}
                                 <ul className="header__right__widget">
-                                <li><i className="fas fa-search"></i></li>
+                                <li><i className="fas fa-search" onClick={handleShow}></i></li>
                                 <li><Link to="/Profile"><i class="fa-regular fa-user"></i></Link></li>
                                 <li><Link to="/Cart"><i className="fas fa-shopping-cart"></i>
                                 </Link></li>
@@ -93,8 +102,36 @@ const Navbar = () => {
                 </div>
             </header>
 
+            <Modal show={show} onHide={handleClose} className="modal">
+                {/* <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header> */}
+                <Modal.Body className="modal_body">
+                    <div className="wrapper">
+                        <div className="container">
+                            <div className="search_wrap search_wrap_3">
+                                <div className="search_box">
+                                    <input type="text" class="input" placeholder="Search..." />
+                                    <div className="btn btn_common">
+                                        <i className="fas fa-search"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Modal.Body>
+                {/* <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer> */}
+            </Modal>
+
         </>
     );
 }
 
-export default Navbar;
+export default Header;
