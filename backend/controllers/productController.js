7b0001367bce8d97c5e7da84dbdd973a2dd1b145
Aditya Das <adityadasofficial31@gmail.com>
2024-01-23 10:18:57 +0530

    product api add

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,7 @@ const catchAsyncError = require('../middleware/catchAsyncError');
 
 // Create a product -- Admin
 
-exports.createProduct = catchAsyncError(async(req, res, next)=>{
+exports.createProduct = catchAsyncError(async (req, res, next) => {
     // Image 
 
     req.body.user = req.user.id;
@@ -18,3 +18,76 @@ exports.createProduct = catchAsyncError(async(req, res, next)=>{
         product,
     });
 });
+
+// Get All Product 
+
+exports.getAllProducts = catchAsyncError(async (req, res, next) => {
+    const products = await Product.find();
+    res.status(200).json({
+        success: true,
+        products,
+    });
+});
+
+
+// Get Product Details
+
+exports.getProductDetails = catchAsyncError(async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return next(new ErrorHandler(`Product not found`, 404));
+    };
+
+    res.status(200).json({
+        success: true,
+        product,
+    });
+
+})
+
+
+// Update Product -- Admin
+
+exports.updateProduct = catchAsyncError(async (req, res, next) => {
+
+    let product = Product.findById(req.params.id);
+
+    if (!product) {
+        return next(new ErrorHandler(`Product not found`, 404));
+    };
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
+        product,
+    });
+
+});
+
+
+// Delete Product -- Admin
+
+exports.deleteProduct = catchAsyncError(async (req, res, next) => {
+
+    let product = Product.findById(req.params.id);
+
+    if (!product) {
+        return next(new ErrorHandler(`Product not found`, 404));
+    };
+
+    // await product.remove();
+    await Product.deleteOne({ _id: req.params.id });
+
+    res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully",
+    });
+});
+
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {isAuthenticatedUser, authorizeRoles} = require("../middleware/auth");
-const { createProduct } = require('../controllers/productController');
+const { createProduct, getAllProducts, updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController');
 
 
 
 router.route("/admin/product/new").post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router.route("/products").get(getAllProducts);
+router.route("/admin/product/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct).delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+router.route("/product/:id").get(getProductDetails);
+
 
 module.exports = router
